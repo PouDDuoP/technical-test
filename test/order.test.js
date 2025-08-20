62a17a7786da773c039a16596ead1953a23a419b
PouDDuoP <kevinalvarado.ag@gmail.com>
2025-08-20 17:11:05 -0400

    Feat: Crear test para productSummaryLinear
    Creado test para validar si regresó la cantidad de elementos deseada y los ítems correspondientes al ejecutar productSummaryLinear

--- test/order.test.js
+++ test/order.test.js
@@ -1,8 +1,37 @@
-import { ORDERS, productSummaryLinear } from '../src/orders.js';
+const { ORDERS, productSummaryLinear } = require('../src/orders.js');
 
-test('summary linear mantiene total de items', () => {
-  const out = productSummaryLinear(ORDERS);
-  const totalQty = out.reduce((a,x)=>a+x.qty,0);
-  const must = ORDERS.flatMap(o=>o.items).reduce((a,x)=>a+x.qty,0);
-  expect(totalQty).toBe(must);
-});
+// test('summary linear mantiene total de items', () => {
+//   const out = productSummaryLinear(ORDERS);
+//   const totalQty = out.reduce((a,x)=>a+x.qty,0);
+//   const must = ORDERS.flatMap(o=>o.items).reduce((a,x)=>a+x.qty,0);
+//   expect(totalQty).toBe(must);
+// });
+
+test('debe retornar objetos con la estructura  sku, qty, and amount', () => {
+  const orders = [
+    {
+      items: [
+        { sku: 'A', qty: 2, price: 10 },
+        { sku: 'B', qty: 1, price: 20 }
+      ]
+    },
+    {
+      items: [
+        { sku: 'A', qty: 1, price: 10 },
+        { sku: 'C', qty: 3, price: 5 }
+      ]
+    }
+  ];
+
+  const result = productSummaryLinear(orders);
+  expect(result.length).toBe(3);
+
+  console.log(result)
+
+  expect(result).toEqual([
+    { sku: 'A', qty: 3, amount: 30 },
+    { sku: 'B', qty: 1, amount: 20 },
+    { sku: 'C', qty: 3, amount: 15 }
+  ]);
+
+})
