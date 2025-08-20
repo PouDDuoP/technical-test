e626ae7f0ead507867d6df3fd42e5be6917aaa41
PouDDuoP <kevinalvarado.ag@gmail.com>
2025-08-20 14:11:22 -0400

    feat: Resolver primer desafío
    Se crearon condiciones para validar los parámetros de entrada y dependiendo de ello retornar los valores o un error, se aplico validación Regex para solo permitir números, se genero el la respuesta de X-Total-Count en el header y se retornan las ordenes segun los parametros page y pageSize.

--- src/routes.js
+++ src/routes.js
@@ -9,12 +9,35 @@ r.get('/me/secure', authSigned, (req, res) => res.json({ id: req.user.id }));
 
 // Desafío B (paginación)
 r.get('/orders', (req, res) => {
-  const page = Math.max(1, parseInt(req.query.page ?? '1', 10));
-  const pageSize = Math.min(100, Math.max(1,
-  parseInt(req.query.pageSize ?? '5', 10)));
-  
+  const page = Math.max(1, parseInt(req.query.page ?? '1', 5));
+  const pageSize = Math.min(100, Math.max(1, parseInt(req.query.pageSize ?? '5', 10)));
+
   // TODO: devolver paginado + header X-Total-Count, validar bordes
-  return res.status(501).json({ error: 'not implemented' });
+  // return res.status(501).json({ error: 'not implemented' });
+
+  const regex = new RegExp(/^\d+$/);
+  const onlyNumber = regex.test(page) || regex.test(pageSize);
+  const totalOrders = ORDERS.length;
+  const oreders = ORDERS.slice((page-1) * pageSize, page * pageSize);
+
+  if (oreders.length === 0) {
+    return res.status(404 ).json({ error: 'no records found, total records available: ' + totalOrders });
+  }
+
+  if (!onlyNumber) {
+    return res.status(400).json({ error: 'Invalid page or pageSize, only numbers are allowed' });
+  }
+
+  if (page < 1 || pageSize < 1) {
+    return res.status(400).json({ error: 'Invalid page or pageSize' });
+  }
+
+  if (page > totalOrders) {
+    return res.status(400).json({ error: 'Page exceeds total number of orders' });
+  }
+
+    res.header('X-Total-Count', totalOrders);
+    res.json( oreders );
 });
 
 // Desafío C (performance)
